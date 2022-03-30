3ae7e89913be16ecb464a4d62db44ed24107d856
unknown <marco995669858@gmail.com>
2022-03-29 21:12:10 -0500

    Se agrega el proyecto

---
+++ src/main/java/com/crud/Actividad2Grupo1Application.java
@@ -0,0 +1,13 @@
+package com.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Actividad2Grupo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Actividad2Grupo1Application.class, args);
+	}
+
+}
